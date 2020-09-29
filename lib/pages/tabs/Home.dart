1f04e9bfdbc86a11e881d1da5b1ea0a657c0864c
mikechang <capapchy@sina.com.cn>
2020-09-29 08:45:18 +0800

    add listvie scroll

--- lib/pages/tabs/Home.dart
+++ lib/pages/tabs/Home.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_swiper/flutter_swiper.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
+//import 'package:flutter_screenutil/flutter_screenutil.dart';
+import '../../services/ScreenAdaper.dart';
 
 class HomePage extends StatefulWidget {
   HomePage({Key key}) : super(key: key);
@@ -36,17 +37,16 @@ class _HomePageState extends State<HomePage> {
     );
   }
 
-
   Widget _titleWidget(value) {
     return Container(
-      height: ScreenUtil.getInstance().setHeight(35),
-      margin: EdgeInsets.only(left: ScreenUtil.getInstance().setWidth(20)),
-      padding: EdgeInsets.only(left: ScreenUtil.getInstance().setWidth(20)),
+      height: ScreenAdaper.height(32),
+      margin: EdgeInsets.only(left: ScreenAdaper.width(20)),
+      padding: EdgeInsets.only(left: ScreenAdaper.width(20)),
       decoration: BoxDecoration(
           border: Border(
               left: BorderSide(
         color: Colors.red,
-        width: ScreenUtil.getInstance().setWidth(10),
+        width: ScreenAdaper.width(10),
       ))),
       child: Text(
         value,
@@ -57,16 +57,45 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
-    ScreenUtil.instance = ScreenUtil(width: 750, height: 1334)..init(context);
-
+    // ScreenUtil.instance = ScreenUtil(width: 750, height: 1334)..init(context);
+    ScreenAdaper.init(context);
     return ListView(
-      children: <Widget>[        
+      children: <Widget>[
         _swiperWidget(),
-        SizedBox(height: 10),
+        SizedBox(height: ScreenAdaper.width(10)),
         _titleWidget("猜你喜欢"),
-         SizedBox(height: 10),
-         _titleWidget("热门推荐"),
+        _hotProductListWidget(),
+        SizedBox(height: ScreenAdaper.width(10)),
+        _titleWidget("热门推荐"),
       ],
     );
+  }
+
+  Widget _hotProductListWidget() {
+    return Container(
+        height: ScreenAdaper.height(234),
+        padding: EdgeInsets.all(ScreenAdaper.width(20)),
+        width: double.infinity,
+        child: ListView.builder(
+          scrollDirection: Axis.horizontal,
+          itemBuilder: (context, index) {
+            return Column(children: <Widget>[
+              Container(
+                  height: ScreenAdaper.height(140),
+                  width: ScreenAdaper.width(140),
+                  margin: EdgeInsets.only(right: ScreenAdaper.width(20)),
+                  child: Image.network(
+                      "https://www.itying.com/images/flutter/hot${index+1}.jpg",
+                      fit: BoxFit.cover)
+              ),
+              Container(
+                padding: EdgeInsets.only(top:ScreenAdaper.height(10)),
+                height: ScreenAdaper.height(44),
+                child: Text("第${index+1}条"),
+              )              
+            ]);
+          },
+          itemCount: 10,
+        ));
   }
 }
